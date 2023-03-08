4f58d6014c7ef9e0209a0511fdec43562f0f50fa
mberndt92 <maximilian_berndt@icloud.com>
2023-03-09 08:23:34 +0900

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Maximilian Berndt on 2023/03/09.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
