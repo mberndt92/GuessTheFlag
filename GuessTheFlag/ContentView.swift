05baf0f41ed9262002e85eff49c34cfa9fcb8d22
mberndt92 <maximilian_berndt@icloud.com>
2023-03-18 17:36:12 +0900

    Add some gradient shenanigans to highlight correctness of the answer

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -57,11 +57,15 @@ struct ContentView: View {
     @State private var animateAnswerCorrectness = false
     @State private var state: GameState = .questionAsked
     
+    private var correctAnswerColor = Color(red: 0.3, green: 0.7, blue: 0.26)
+    private var wrongAnswerColor = Color(red: 0.76, green: 0.15, blue: 0.26)
+    private var askingQuestionColor = Color(red: 0.1, green: 0.2, blue: 0.45)
+    
     var body: some View {
         ZStack {
             RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+                .init(color: askingQuestionColor, location: 0.3),
+                .init(color: gradientColor(), location: 0.3)
             ], center: .top, startRadius: 200, endRadius: 700)
             .ignoresSafeArea()
             VStack {
@@ -95,7 +99,6 @@ struct ContentView: View {
                 }
                 .frame(maxWidth: .infinity)
                 .padding(.vertical, 20)
-//                .background(state != .questionAsked ? .regularMaterial : selectedAnswer == correctAnswer ? .red : .green)
                 .background(.regularMaterial)
                 .clipShape(RoundedRectangle(cornerRadius: 20))
                 Spacer()
@@ -125,6 +128,11 @@ struct ContentView: View {
             print("gameOver")
             showingNewGame = true
         }
+    }
+    
+    private func gradientColor() -> Color {
+        let answerColor = selectedAnswer == correctAnswer ? correctAnswerColor : wrongAnswerColor
+        return state == .questionAsked ? askingQuestionColor : answerColor
     }
     
     private func evaluateAnswer(_ number: Int) {
